eac377e7f4c72cefacd5cd67312312d4115567b9
maaush <mauribenitez95@gmail.com>
2020-04-04 01:51:36 -0300

    finish third exercise.

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejercicio3
+{
+    class Program
+
+    {
+        public const int People = 10;
+        
+        static void Main(string[] args)
+        {
+            
+            /*
+             * Objetivo: Comprender el uso basico de estructuras repetitivas.
+             * Ejercicio:
+             *     - Calcular la media y desviacion estandar de un conjunto de 10 personas.
+             *     - Tome por teclado la altura en cm de cada persona y carguela a un arreglo.
+             *     - Presente los resultados obtenidos.
+             *     - Muestre que alturas se encuentran por encima de la media y por debajo de ella.
+             *     - Muestre que alturas se encuentran dentro del rango definido por la desviacion estandar.
+             */
+
+            decimal[] heights = new decimal[People];
+
+            heights = GetHeights(heights);
+            PrintArray(heights);
+            
+            decimal average = GetAverage(heights);
+            decimal standardDeviation = GetStandardDeviation(average, heights);
+
+            var belowAverage =
+                from height in heights
+                where height < average
+                select height;
+
+            var aboveAverage =
+                from height in heights
+                where height >= average
+                select height;
+
+            var inRangeBetweenDeviation =
+                from height in heights
+                where height > 0 && height <= standardDeviation
+                select height;
+
+            Console.WriteLine("Heights equal to or above the average height in heights: ");
+            PrintAboveAverageValues(aboveAverage);
+
+            Console.WriteLine();
+            Console.WriteLine("Heights below to the average height in heights: ");
+            PrintBelowAverageValues(belowAverage);
+
+            Console.WriteLine();
+            Console.WriteLine($"Heights between the range of 0 and {standardDeviation}");
+            PrintAboveAverageValues(inRangeBetweenDeviation);
+        }
+
+        private static decimal[] GetHeights(decimal[] arrayOfHeights)
+        {
+
+            for (int i = 0; i < People; i++)
+            {
+                Console.WriteLine("Input a height (in centimeters): ");
+                arrayOfHeights[i] = Convert.ToDecimal(Console.Read());
+                Console.WriteLine();
+            }
+
+            return arrayOfHeights;
+        }
+
+        private static void PrintArray(decimal[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+        }
+
+        private static decimal GetAverage(decimal[] array)
+        {
+            decimal avg = 0;
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                avg += array[i];
+            }
+
+            return avg / array.Length;
+        }
+
+        private static decimal GetStandardDeviation(decimal average, decimal[] arrayOfData)
+        {
+
+            decimal[] arrayOfDeviation = new decimal[arrayOfData.Length];
+            decimal total = 0;
+
+            for (int i = 0; i < arrayOfData.Length; i++)
+            {
+                double firstDeviation = (double) arrayOfData[i] - (double) average;
+                arrayOfDeviation[i] = Math.Abs((decimal) Math.Pow(firstDeviation, 2));
+            }
+            
+            foreach (decimal value in arrayOfDeviation)
+            {
+                total += value;
+            }
+
+            return total / arrayOfDeviation.Length;
+        }
+
+        private static void PrintAboveAverageValues(IEnumerable<decimal> enumerable)
+        {
+            Console.WriteLine("Heights equal to or above the average height: ");
+            foreach (decimal value in enumerable)
+            {
+                Console.Write($"{value}, ");   
+            }
+        }
+
+        private static void PrintBelowAverageValues(IEnumerable<decimal> enumerable)
+        {
+            
+            Console.WriteLine("Heights equal to or above the average height: ");
+            foreach (decimal value in enumerable)
+            {
+                Console.Write($"{value}, ");   
+            }
+        }
+    }
+}
