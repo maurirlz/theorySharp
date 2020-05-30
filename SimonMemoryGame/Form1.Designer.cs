aa619429030254a1de6c9f8b1b9371b64737b441
Mauri Benitez <mauribenitez95@gmail.com>
2020-05-30 05:02:32 -0300

    Finish GUI App about simon says

---
+++ SimonMemoryGame/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SimonMemoryGame
+{
+    partial class SimoneForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimoneForm));
+            this.Blue = new System.Windows.Forms.Button();
+            this.Green = new System.Windows.Forms.Button();
+            this.Yellow = new System.Windows.Forms.Button();
+            this.Red = new System.Windows.Forms.Button();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Blue
+            // 
+            this.Blue.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Blue.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Blue.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Blue.FlatAppearance.BorderSize = 3;
+            this.Blue.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Blue.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Blue.Location = new System.Drawing.Point(489, 294);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(315, 200);
+            this.Blue.TabIndex = 1;
+            this.Blue.Text = "Blue";
+            this.Blue.UseVisualStyleBackColor = false;
+            this.Blue.Click += new System.EventHandler(this.Blue_Click);
+            // 
+            // Green
+            // 
+            this.Green.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Green.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Green.FlatAppearance.BorderSize = 3;
+            this.Green.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Green.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Green.Location = new System.Drawing.Point(63, 41);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(315, 198);
+            this.Green.TabIndex = 3;
+            this.Green.Text = "Green";
+            this.Green.UseVisualStyleBackColor = false;
+            this.Green.Click += new System.EventHandler(this.Green_Click);
+            // 
+            // Yellow
+            // 
+            this.Yellow.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Yellow.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Yellow.FlatAppearance.BorderSize = 3;
+            this.Yellow.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Yellow.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Yellow.Location = new System.Drawing.Point(63, 294);
+            this.Yellow.Name = "Yellow";
+            this.Yellow.Size = new System.Drawing.Size(315, 198);
+            this.Yellow.TabIndex = 5;
+            this.Yellow.Text = "Yellow";
+            this.Yellow.UseVisualStyleBackColor = false;
+            this.Yellow.Click += new System.EventHandler(this.Yellow_Click);
+            // 
+            // Red
+            // 
+            this.Red.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Red.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Red.FlatAppearance.BorderSize = 3;
+            this.Red.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Red.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Red.Location = new System.Drawing.Point(489, 39);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(315, 200);
+            this.Red.TabIndex = 4;
+            this.Red.Text = "Red";
+            this.Red.UseVisualStyleBackColor = false;
+            this.Red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ScoreLabel.Font = new System.Drawing.Font("Lucida Console", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ScoreLabel.Location = new System.Drawing.Point(370, 506);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(130, 24);
+            this.ScoreLabel.TabIndex = 6;
+            this.ScoreLabel.Text = "Score: 0";
+            // 
+            // SimoneForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(894, 539);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.Yellow);
+            this.Controls.Add(this.Red);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Blue);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "SimoneForm";
+            this.Text = "Simon Memory Game - Mauricio Ezequiel Benitez, Frias Lucas, Garavaglia Mauricio, " +
+    "Bruzzo Fernando";
+            this.Load += new System.EventHandler(this.SimoneForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Blue;
+        private System.Windows.Forms.Button Green;
+        private System.Windows.Forms.Button Yellow;
+        private System.Windows.Forms.Button Red;
+        private System.Windows.Forms.Label ScoreLabel;
+    }
+}
+
---
+++ SimonMemoryGame/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SimonMemoryGame
+{
+    public partial class SimoneForm : Form
+    {
+        // Entero que representa el numero de rondas que va el jugador
+
+        private int roundNumber;
+
+        // Booleano que se usa para flaggear si se esta replicando el patron de la lista en los botones (simon esta hablando)
+
+        private bool isSimonSpeaking;
+
+        // Lista de enteros que conforma el patron entero, todos los enteros de esta lista solo pueden contener desde el numero 0 al 4 gracias a Random.
+
+        private List<int> colorPattern;
+
+        // Random para generar lo que simon va a decir (elementos del patron)
+        // va desde 0 a 4, para representar cada color (Rojo = 0, Azul = 1, Amarillo = 2, Verde = 3)
+
+        private Random randomGenerator;
+
+
+        // Inicializacion de lista y instancia de random (lista tiene que estar inicializada para albergar numeros
+        // Random tiene que estar inicializado para que genere numeros random.
+
+        public SimoneForm()
+        {
+            InitializeComponent();
+            colorPattern = new List<int>();
+            randomGenerator = new Random();
+        }
+
+        // por cada eleccion / boton que tiene el jugador, si el jugador clickea dicho boton llama a esta funcion dependiendo del color que representa el boton
+        // que clickeo, de ahi chequeamos: 
+
+        void CheckIfPlayerChoiceIsCorrect(int numberColor)
+        {
+
+            // Si simon esta hablando, retornamos, no es turno del jugador.
+
+            if (isSimonSpeaking) return;
+
+            // Explicacion: la lista contiene colores, y cada posicion de la lista corresponde a un numero de ronda, por ej: ronda 1 rojo, lista[rojo], ronda 2 azul lista[rojo,azul]... etc
+            // entonces se puede decir que la ronda 1 tiene el color rojo, la ronda 2 el azul, y asi, entonces, aca chequeamos si el jugador apreto un boton el cual corresponde
+            // a la ronda que actualmente esta intentando adivinar, si es asi, seguimos a la proxima ronda (el patron es obviamente, al ser la lista, un conjunto de rondas.)
+
+            if (colorPattern[roundNumber] == numberColor)
+            {
+                roundNumber++;
+            }
+            else // si el jugador adivino incorrectamente la ronda que estaba adivinando, pierde
+            {
+                MessageBox.Show($"You failed, final score: {colorPattern.Count.ToString()}", "GAME OVER");
+                roundNumber = 0;
+                colorPattern = new List<int>();
+                new Thread(StartASimonRound).Start();
+            }
+
+            // Si el padron entero, que es un conjunto de rondas, fue adivinado por completo,
+            // significa que round number llego al mismo valor numerico que las posiciones de la lista (porque basicamente, recorrio la lista entera.
+            // esto significa que el patron completo fue correctamente replicado, por lo cual, agregamos un nuevo color a la lista (lo cual cambia el patron)
+            // seteamos el numero de rondas a 0 para que se tenga que replicar el patron desde el comienzo y se llama a un nuevo hilo a que ejecute otra ronda.
+
+            if (roundNumber >= colorPattern.Count)
+            {
+                colorPattern.Add(randomGenerator.Next(0, 4));
+                roundNumber = 0;
+                new Thread(StartASimonRound).Start();
+            }
+
+            // seteamos el valor del label al total de rondas adivinadas, restando 1 porque como los indices de los arrays, los de la lista tambien son basados en 0, o 0 based.
+
+            ScoreLabel.Text = ($"Score: {(colorPattern.Count - 1).ToString()}");
+        }
+
+
+        // la ronda siempre empieza con " simon " diciendo el patron que tiene albergado en la lista
+
+        void StartASimonRound()
+        {
+            // para evitar que los hilos se interfieran, si un hilo llama por error a StartASimonRound() (o el jugador clickea un boton mientras simon habla)
+            // y simon esta hablando, Cuando Se chequea la respuesta directamente retorna porque no es el turno del jugador.
+
+            isSimonSpeaking = true;
+
+            // por cada color que tenemos en el patron/lista -> chequeamos el color que tenemos en la iteracion correspondiente
+            // -> cambiamos el color del boton que representa ese color, llamamos a dormir al hilo correspondiente por 190milisegundos
+            // -> volvemos a cambiar al =color normal, dando el efecto de que simon " esta hablando " y repitiendo el patron.
+
+            foreach (int colorNumber in colorPattern)
+            {
+                switch (colorNumber)
+                {
+                    case 0:
+
+                        Red.BackColor = Color.Red;
+                        Thread.Sleep(190);
+                        Red.BackColor = Color.Black;
+
+                        break;
+                    case 1:
+
+                        Blue.BackColor = Color.Blue;
+                        Thread.Sleep(190);
+                        Blue.BackColor = Color.Black;
+
+                        break;
+                    case 2:
+
+                        Yellow.BackColor = Color.Yellow;
+                        Thread.Sleep(190);
+                        Yellow.BackColor = Color.Black;
+
+                        break;
+                    case 3:
+
+                        Green.BackColor = Color.Green;
+                        Thread.Sleep(190);
+                        Green.BackColor = Color.Black;
+                        break;
+                }
+
+                Thread.Sleep(200);
+            }
+
+            // Simon dejo de hablar / se dejo de reproducir el patron, setteamos la bandera en falso.
+
+            isSimonSpeaking = false;
+        }
+
+        // onClickListeners / clickHandlers / funciones que manejan los eventos de click de cada boton
+
+        private void Red_Click(object sender, EventArgs e)
+        {
+            CheckIfPlayerChoiceIsCorrect(0);
+        }
+
+        private void Blue_Click(object sender, EventArgs e)
+        {
+            CheckIfPlayerChoiceIsCorrect(1);
+        }
+
+        private void Yellow_Click(object sender, EventArgs e)
+        { 
+            CheckIfPlayerChoiceIsCorrect(2);
+        }
+
+        private void Green_Click(object sender, EventArgs e)
+        {        
+            CheckIfPlayerChoiceIsCorrect(3);
+        }
+
+
+        // Cuando se inicializa la forma, agregamos el primero numero al patron y empezamos el juego con un nuevo hilo.
+
+        private void SimoneForm_Load(object sender, EventArgs e)
+        {
+            colorPattern.Add(randomGenerator.Next(0, 4));
+            new Thread(StartASimonRound).Start();
+        }
+    }
+}
+
